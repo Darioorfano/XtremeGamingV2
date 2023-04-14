9cd4c06154271dbb5751513ad329cb6a3b95b159
Dario <darioorfano@hotmail.com>
2023-04-14 18:18:13 -0300

    Se realizo mejora de Ui para la vista Home, se incorporo efecto sobre botones en el footer

--- src/assets/js/owl.js
+++ src/assets/js/owl.js
@@ -3,30 +3,28 @@ $(document).ready(function () {
     autoplay: true,
     margin: 0,
     items: 4,
-    nav: true,
     doops: true,
-
     responsive: {
       0: {
         items: 1,
         dots: true,
-        nav:false,
       },
       480: {
         margin: 80,
         items:1,
         dot: false,
-        nav: false,
+        nav: true,
       },
       768: {
         items: 2,
-        nav: true,
+        nav:true
       },
       992: {
         items: 3,
       },
       1200: {
         items: 4,
+        nav:true
       },
     },
   });
