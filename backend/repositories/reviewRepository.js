2df21da3738641da93c40f5667d9bf4bed03f94a
21diego <morinigodiego21@gmail.com>
2023-08-04 23:04:17 -0300

    add method for get reviews

--- backend/repositories/reviewRepository.js
+++ backend/repositories/reviewRepository.js
@@ -1,5 +1,25 @@
 const {db} = require('../config/firebase');
-const { addDoc, collection } = require("firebase/firestore"); 
+const { addDoc, collection, query, getDocs, where } = require("firebase/firestore"); 
+
+const getReviewsFromProduct = async (idProduct) => {
+    try {
+        const listReviews = []
+        const q = query(collection(db, "reviews"), where("idProducto", "==", idProduct));
+
+        const querySnapshot = await getDocs(q);
+
+        querySnapshot.forEach(doc => {
+            let review = { id: doc.id, ...doc.data()}
+            listReviews.push(review);
+        });
+    
+        return listReviews;
+
+    } catch (error) {
+        console.log(error)
+        return {code: 500, mensaje: error };
+    }
+}
 
 const postReview = async (name, rating, comment, idUser, idProducto, imageUrl) => {
     try{
@@ -9,11 +29,12 @@ const postReview = async (name, rating, comment, idUser, idProducto, imageUrl) =
         return {code: 201, mensaje: `La resenia ${docRef.id} se ha creado con exito` };
 
     } catch (error){
-        console.log(error)
+        return {code: 500, mensaje: error };
     }
       
 }
 
 module.exports = {
-    postReview
+    postReview,
+    getReviewsFromProduct
 }
