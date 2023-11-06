a43de7a47b1f795dcd0d3b7495e6e2afc1cd94be
Dario <darioorfano@hotmail.com>
2023-11-06 13:12:54 -0300

    Separacion de proyecto

--- backend/routes/cartRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const cartController = require('../controllers/cartController');
-
-router.post('/comprar', cartController.buyCart);
-router.get('/mis-compras/:idUsuario', cartController.getPurchasesByUser);
-
-module.exports = router;
--- backend/routes/reviewRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const previewController = require('../controllers/reviewController');
-
-// Rutas relacionadas con los usuarios
-router.get('/getReviews/:idProduct', previewController.getReviewsFromProduct);
-router.post('/add-review', previewController.postReview);
-module.exports = router;
